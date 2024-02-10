51fa91d215d504288214b60e9d0bf330af144ee3
Shubham <shubham>
2024-02-10 09:29:58 +0530

    dashboard added

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,14 +1,54 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import React, { useState } from 'react'
+import Layout from '../../components/layout'
+import { Button, Card, Text } from 'react-native-paper';
+
+
+
+
 
 const Dashboard = () => {
+
+    const handleChange = (key, value) => {
+        console.log({ key, value })
+    }
+
+
     return (
-        <View>
-            <Text>Dashboard</Text>
-        </View>
+        <Layout>
+            <Card onPress={handleChange} style={styles.card}>
+                <Card.Content>
+                    <Text variant="titleLarge">  title</Text>
+                    <Text variant="bodyMedium">  content</Text>
+                </Card.Content>
+                <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+                <Card.Actions>
+                    <Button>Cancel</Button>
+                    <Button>Ok</Button>
+                </Card.Actions>
+            </Card>
+
+        </Layout>
     )
 }
 
 export default Dashboard
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    input: {
+        width: '100%',
+        marginVertical: 10,
+    },
+    btn: {
+        width: '100%',
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginTop: 20,
+        // position: 'absolute',
+        // bottom: 20
+    },
+    card: {
+        width: '100%',
+        marginVertical: 10
+    }
+})
